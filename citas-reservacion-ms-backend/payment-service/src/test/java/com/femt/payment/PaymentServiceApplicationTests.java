6601781231fed3960e74a7dcdd893be4f6590d6d
MenesesTech <menesesfrey@gmail.com>
2025-07-09 16:20:11 -0500

    Microservicio para la gestion de pagos

---
+++ citas-reservacion-ms-backend/payment-service/src/test/java/com/femt/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femt.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
