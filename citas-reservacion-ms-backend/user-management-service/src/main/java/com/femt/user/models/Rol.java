f2d6bdf5068da4e28567d8dc7f8a7b98eb20c31e
MenesesTech <menesesfrey@gmail.com>
2025-07-09 18:38:15 -0500

    Agregar modelos de usuario y rol para la gestion de usuarios

---
+++ citas-reservacion-ms-backend/user-management-service/src/main/java/com/femt/user/models/Rol.java
@@ -0,0 +1,16 @@
+package com.femt.user.models;
+
+public enum Rol {
+    ADMIN("admin"),
+    USER("user");
+
+    private String rol;
+
+    Rol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getRol() {
+        return this.rol;
+    }
+}
---
+++ citas-reservacion-ms-backend/user-management-service/src/main/java/com/femt/user/models/Usuario.java
@@ -0,0 +1,62 @@
+package com.femt.user.models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario implements UserDetails {
+    private String id;
+    private String name;
+    private String dni;
+    private String email;
+    private String password;
+    private Rol rol;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+        if (this.rol == Rol.ADMIN)
+            roles.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
