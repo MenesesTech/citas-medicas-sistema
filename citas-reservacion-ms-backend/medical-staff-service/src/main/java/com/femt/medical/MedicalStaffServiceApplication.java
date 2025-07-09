e3aa6ed728b7a2bd1ea537ea0cc7e8b6f1fd164c
MenesesTech <menesesfrey@gmail.com>
2025-07-09 15:51:03 -0500

    Microsericio para la gestion de personal medico

---
+++ citas-reservacion-ms-backend/medical-staff-service/src/main/java/com/femt/medical/MedicalStaffServiceApplication.java
@@ -0,0 +1,13 @@
+package com.femt.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalStaffServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalStaffServiceApplication.class, args);
+	}
+
+}
