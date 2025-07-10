2f6d337e98b02c80de7b54dac6e60718040d83d0
MenesesTech <menesesfrey@gmail.com>
2025-07-09 23:55:37 -0500

    Agregar entidad usuario y Rol

--- citas-reservacion-ms-backend/user-management-service/src/main/java/com/femt/user/models/Usuario.java
+++ citas-reservacion-ms-backend/user-management-service/src/main/java/com/femt/user/models/Usuario.java
@@ -7,10 +7,16 @@ import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -18,13 +24,34 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of = "id") // Esta anotacion de lombok debe usarse para comparar objetos de tipo Usuario
 public class Usuario implements UserDetails {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String dni;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private Rol rol;
+
+    public Usuario(String name, String dni, String email, String password, Rol rol) {
+        this.name = name;
+        this.dni = dni;
+        this.email = email;
+        this.password = password;
+        this.rol = rol;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
